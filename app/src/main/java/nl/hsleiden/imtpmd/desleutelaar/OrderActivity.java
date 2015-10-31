5ea63ccb60976f1b893db78a441a674ad18003eb
Marinosbitter <marijn_bitter@live.nl>
2015-10-31 17:00:28 +0100

    Customer details are autofilled

--- app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
+++ app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
@@ -1,6 +1,8 @@
 package nl.hsleiden.imtpmd.desleutelaar;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +12,42 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.hsleiden.imtpmd.desleutelaar.database.DatabaseHelper;
+import nl.hsleiden.imtpmd.desleutelaar.database.DatabaseInfo;
+
 public class OrderActivity extends AppCompatActivity {
+    DatabaseHelper dbHelper = DatabaseHelper.getHelper(this);
+
+    EditText namefield;
+    EditText addressfield;
+    EditText telephonefield;
+    EditText emailfield;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        namefield = (EditText) findViewById(R.id.nameField);
+        addressfield = (EditText) findViewById(R.id.addressField);
+        telephonefield = (EditText) findViewById(R.id.telephoneField);
+        emailfield = (EditText) findViewById(R.id.emailField);
+
+        Cursor customerRS = dbHelper.query(DatabaseInfo.CustomerTables.CUSTOMER, new String[]{"*"}, null, null, null, null, null);
+
+        if (customerRS.getCount() != 0) {
+            customerRS.moveToFirst();   // Skip de lege elementen vooraan de rij. Maar : rij kan leeg zijn dus falen
+
+            namefield.setText(customerRS.getString(customerRS.getColumnIndex(DatabaseInfo.CustomerColumn.CUSTOMERNAME)));
+            addressfield.setText(customerRS.getString(customerRS.getColumnIndex(DatabaseInfo.CustomerColumn.ADRESS)));
+            telephonefield.setText(customerRS.getString(customerRS.getColumnIndex(DatabaseInfo.CustomerColumn.PHONE)));
+            emailfield.setText(customerRS.getString(customerRS.getColumnIndex(DatabaseInfo.CustomerColumn.MAIL)));
+        }
     }
 
     public void openLockInfoActivity(View view) {
@@ -46,19 +78,19 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     public void confirmOrder(View view) {
-        EditText namefield = (EditText)findViewById(R.id.nameField);
         String name = namefield.getText().toString();
-
-        EditText addressfield = (EditText)findViewById(R.id.addressField);
         String address = addressfield.getText().toString();
-
-        EditText telephonefield = (EditText)findViewById(R.id.telephoneField);
         String telephone = telephonefield.getText().toString();
-
-        EditText emailfield = (EditText)findViewById(R.id.emailField);
         String email = emailfield.getText().toString();
 
-        // even testen
-        Log.d("Gegevens:", name + " " + address + " " + telephone + " " + email);
+        ContentValues values = new ContentValues();
+        values.put(DatabaseInfo.CustomerColumn.CUSTOMERNAME, "" + name);
+        values.put(DatabaseInfo.CustomerColumn.ADRESS, "" + address);
+        values.put(DatabaseInfo.CustomerColumn.PHONE, "" + telephone);
+        values.put(DatabaseInfo.CustomerColumn.MAIL, "" + email);
+        dbHelper.insert(DatabaseInfo.CustomerTables.CUSTOMER, null, values);
+
+        String orderJSON = "{customerName: '"+name+"', adress: '"+address+"', phone: '"+telephone+"', email: '"+email+"', type: 'sf'}]";
+        // Use orderJSON to communicate with the database api :)
     }
 }
