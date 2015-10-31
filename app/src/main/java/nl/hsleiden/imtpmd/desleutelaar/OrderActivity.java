740abc29b85629a60b859ab43d5c99e890b83a26
ToonBrand <toonbrand@hotmail.com>
2015-10-31 16:27:49 +0100

    Toast notification toegevoegd. Werkende navigatie. Slotnaam word naar elke activity doorgegeven.

--- app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
+++ app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
@@ -1,5 +1,6 @@
 package nl.hsleiden.imtpmd.desleutelaar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,41 +10,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "nl.hsleiden.imtpmd.desleutelaar.MESSAGE";
+    String itemValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+        itemValue = intent.getStringExtra(LockInfoActivity.EXTRA_MESSAGE);
+
+        TextView lockName;
+        lockName = (TextView) findViewById(R.id.lockInfoField);
+        lockName.setText(itemValue);
     }
 
     public void openLockInfoActivity(View view) {
         Intent intent = new Intent(this, LockInfoActivity.class);
+        String message = itemValue;
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_order, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void confirmOrder(View view) {
         EditText namefield = (EditText)findViewById(R.id.nameField);
@@ -60,5 +53,12 @@ public class OrderActivity extends AppCompatActivity {
 
         // even testen
         Log.d("Gegevens:", name + " " + address + " " + telephone + " " + email);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Bestelling geplaatst!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
