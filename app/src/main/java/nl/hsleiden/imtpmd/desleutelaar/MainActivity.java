ce8344c4761b3b091d83ff7dbb4c57d2ce4aadc0
Marinosbitter <marijn_bitter@live.nl>
2015-10-31 14:05:36 +0100

    Small change in semantics

--- app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/MainActivity.java
+++ app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/MainActivity.java
@@ -95,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-
         // Assign adapter to ListView
         listView.setAdapter(adapter);
 
