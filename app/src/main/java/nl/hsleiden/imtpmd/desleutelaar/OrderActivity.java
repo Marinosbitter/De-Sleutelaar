a17132c3caed7781d18cdbc76d093b03755853ca
ToonBrand <toonbrand@hotmail.com>
2015-10-31 17:11:14 +0100

    Kleine aanpassingen in strings en dergelijke.

--- app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
+++ app/src/main/java/nl/hsleiden/imtpmd/desleutelaar/OrderActivity.java
@@ -36,9 +36,8 @@ public class OrderActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-
     public void confirmOrder(View view) {
+        // haal de waarden uit de velden
         EditText namefield = (EditText)findViewById(R.id.nameField);
         String name = namefield.getText().toString();
 
@@ -54,11 +53,15 @@ public class OrderActivity extends AppCompatActivity {
         // even testen
         Log.d("Gegevens:", name + " " + address + " " + telephone + " " + email);
 
+        // laat een toast zien
         Context context = getApplicationContext();
         CharSequence text = "Bestelling geplaatst!";
         int duration = Toast.LENGTH_SHORT;
-
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+
+        // ga terug naar hoofdscherm
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
